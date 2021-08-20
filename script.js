946f5ad5357518ac95b2c2b87492c9f591fb6e84
Kyle Lux <kyleslux86@gmail.com>
2021-08-19 23:46:49 -0400

    Structure of page and start of scripts

--- script.js
+++ script.js
@@ -1,1 +1,47 @@
+var rootEl = document.getElementById('root');
+var containerEl = document.getElementById('main-container');
 
+
+
+var scores = [];
+
+
+function clearContainer() {
+    containerEl.innerHTML = '';
+}
+
+function showScores() {
+    clearContainer();
+
+    for (var i = 0; i < scores.length; i++) {
+        var score = scores[i];
+    }
+}
+
+function showWelcome() {
+
+}
+
+function startQuiz() {
+
+}
+
+function nextQuestion() {
+
+}
+
+function chackAnswer() {
+
+}
+
+function quizTimer() {
+
+}
+
+function answerTimer() {
+
+}
+
+function init() {
+    
+}
